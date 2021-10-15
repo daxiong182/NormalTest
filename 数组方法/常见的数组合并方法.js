a94f490a2d12f7494cfea53080926efa1b5a8567
daxiong182 <2043693318@qq.com>
2021-10-16 02:05:12 +0800

    m

---
+++ 数组方法/常见的数组合并方法.js
@@ -0,0 +1,12 @@
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5, 6]
+
+// const result = arr1.push(arr2)
+
+// let arr3 = arr1.flat()
+
+// console.log(arr3)
+
+
+arr1.push.apply(arr1, arr2);
+console.log(arr1);
